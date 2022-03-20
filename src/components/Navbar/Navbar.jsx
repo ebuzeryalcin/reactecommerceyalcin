92c16925997f3304db386789ec8479a0545aeb35
Ebuzer Yalcin <ebuzeryalcin@Macbook-m1.local>
2022-03-20 19:15:01 +0100

    project completed commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core';
+import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
+
+import logo from '../../assets/archive.png'
+import useStyles from './styles';
+
+const Navbar = ({ totalItems }) => {
+  const classes = useStyles();
+  const location = useLocation();
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+            <img src={logo} alt="Tbtv-store.js" height="50px" className={classes.image} />
+            Archive
+          </Typography>
+          <div className={classes.grow} />
+          {location.pathname === '/' && (
+            <div className={classes.button}>
+            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+};
+
+export default Navbar;
